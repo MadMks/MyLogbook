8b3cca4cd4568d3f1141d23ce2aff0f9b820fd53
Makc <mks59k@gmail.com>
2019-03-12 21:14:45 +0200

    Добавлена модель преподавателя.

---
+++ MyLogbook.Repositories/Interfaces/ITeacherRepository.cs
@@ -0,0 +1,12 @@
+﻿using MyLogbook.Abstractions;
+using MyLogbook.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLogbook.Repositories.Interfaces
+{
+    public interface ITeacherRepository : IDbRepository<Teacher>
+    {
+    }
+}
